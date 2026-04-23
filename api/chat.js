c17c3a790c89248e99846662000b98b674558063
MekraPharma <146339562+MekraPharma@users.noreply.github.com>
2026-04-23 18:13:25 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const { message } = req.body;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +
         process.env.GEMINI_API_KEY,
       {
         method: "POST",
